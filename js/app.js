1d4ec8d839dc20ddab3fc04dde5b989590aee11d
SanyFaysal <tfoysalahmed@gmail.com>
2022-03-11 16:24:43 +0600

    add to like added

--- js/app.js
+++ js/app.js
@@ -12,13 +12,12 @@ const getReportedPosts = () => {
 };
 
 const isLiked = (id) => {
-
   return likedPostsId?.length && !!likedPostsId.includes(id);
 };
 
 const addToLiked = (id) => {
   likedPostsId.push(id);
-  showPosts(likedPostsId);
+  showPosts(posts);
 };
 
 const reportPost = (id) => {
@@ -28,7 +27,7 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
 const switchTab = (id) => {
@@ -67,10 +66,12 @@ const createPost = (post) => {
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
+
                 <button class="post__more-options">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
               </div>
+
               <div class="post__content">
                 <div class="post__medias">
                   <img
@@ -80,31 +81,41 @@ const createPost = (post) => {
                   />
                 </div>
               </div>
+
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
-                  <i class="fa-solid fa-heart ${isLiked(post.id && "text-danger")}"></i>
+                  <i class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
+                    
                   </button>
                   <button class="post__button">
                     <i class="fa-solid fa-comment"></i>
                   </button>
                   
+
                   <div class="post__indicators"></div>
-                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id})">
+
+                  <button class="post__button post__button--align-right" onclick="reportPost(${post.id
+    })">
                     <i class="fa-solid fa-ban"></i>
                   </button>
                 </div>
+
                 <div class="post__content">${displayContent(post.description)}</div>
+
                 <div class="post__infos">
                   <div class="post__likes">
                     <a href="#" class="post__likes-avatar">
                       <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="User Picture" />
                     </a>
+
                     <span>Liked by
                       <a class="post__name--underline" href="#">user123</a> and
                       <a href="#">73 others</a></span>
                   </div>
+
                   <hr/>
+
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
@@ -131,7 +142,6 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
-
   const likedPosts = getLikedPosts();
   likedPosts.forEach((post) => {
     const div = createPost(post);
